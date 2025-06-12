f94f01fee5142371b523b03bec57ed6709f6fcdb
Vibhu Tripathi <vibhu@radiantsolution.co>
2025-06-12 14:29:42 +0530

    OAuth 2 basic

---
+++ src/main/java/com/vibhu/security/HomeController.java
@@ -0,0 +1,18 @@
+package com.vibhu.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal OAuth2User user) {
+        model.addAttribute("user", user);
+        return "home";
+    }
+}
+
